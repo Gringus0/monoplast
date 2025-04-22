88228b8948e19885baca0d0a1b4b018b8c7b2501
Gringus0~ <aleksandar.acko.jovanovic@gmail.com>
2025-04-22 21:42:08 +0200

    Added reminders to prices.blade.php

--- resources/views/pages/products/prices.blade.php
+++ resources/views/pages/products/prices.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.layout')
 @section('content')
 Prices Page
+{{--    @dd($bananaBezOjacanja)--}}
+{{--    @dd($bananaOjacanaIliFleksibiln~~~a)--}}
+{{--    @dd($bananaOjacanaIliFleksibilnaIFalt)--}}
+{{--    @dd($bananaBezOjacanjaIFalt)--}}
+{{--    @dd($blankoBezOjacanja)--}}
+{{--    @dd($blankoOjacanaIFalt)--}}
+
+{{--    @foreach($bananaBezOjacanja as $kesa)--}}
+{{--        <br>{{$kesa->boja3}}--}}
+{{--    @endforeach--}}
 @endsection
