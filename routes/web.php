ac084fddbb7ca44f2ef7ed6d7bf05e49984fff05
Gringus0~ <aleksandar.acko.jovanovic@gmail.com>
2025-04-22 21:44:24 +0200

    Deleted unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Route::get('/', HomeController::class . '@home')->name('home');
 Route::get('/about', HomeController::class . '@about')->name('about');
 Route::get('/products', ProductController::class . '@listProductsWithGallery')->name('productsWithGallery');
