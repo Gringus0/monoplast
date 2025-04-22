46598cea19e42ca19bda564bf0611347be9b38a7
Gringus0~ <aleksandar.acko.jovanovic@gmail.com>
2025-04-22 21:40:41 +0200

    Corrected the name of BlankoOjacanaFalt.php

--- app/Models/BlankoOjacanaIFalt.php
+++ app/Models/BlankoOjacanaIFalt.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class BlankoOjacaneIFalt extends Model
+class BlankoOjacanaIFalt extends Model
 {
     // grupa 6
     protected $table = 'grupa6';
