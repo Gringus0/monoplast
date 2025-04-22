5d437684f606df0b88dc7232c5875c8c761bd651
Gringus0~ <aleksandar.acko.jovanovic@gmail.com>
2025-04-22 21:43:52 +0200

    Calculated prices for bags and sent to front

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BananaBezOjacanja;
+use App\Models\BananaBezOjacanjaIFalt;
+use App\Models\BananaOjacanaIliFleksibilna;
+use App\Models\BananaOjacanaIliFleksibilnaIFalt;
+use App\Models\BlankoBezOjacanja;
+use App\Models\BlankoOjacanaIFalt;
+use App\Models\Bod;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -11,6 +18,42 @@ class ProductController extends Controller
     }
 
     public function listProductPrices() {
-        return view('pages.products.prices');
+        $bananaBezOjacanja = BananaBezOjacanja::get();
+        $bananaOjacanaIliFleksibilna = BananaOjacanaIliFleksibilna::get();
+        $bananaOjacanaIliFleksibilnaIFalt = BananaOjacanaIliFleksibilnaIFalt::get();
+        $bananaBezOjacanjaIFalt = BananaBezOjacanjaIFalt::get();
+        $blankoBezOjacanja = BlankoBezOjacanja::get();
+        $blankoOjacanaIFalt = BlankoOjacanaIFalt::get();
+
+        $vrednostBoda = Bod::first()->vrednost;
+
+        $this->calculatePrices($bananaBezOjacanja, $vrednostBoda);
+        $this->calculatePrices($bananaOjacanaIliFleksibilna, $vrednostBoda);
+        $this->calculatePrices($bananaOjacanaIliFleksibilnaIFalt, $vrednostBoda);
+        $this->calculatePrices($bananaBezOjacanjaIFalt, $vrednostBoda);
+        $this->calculatePrices($blankoBezOjacanja, $vrednostBoda);
+        $this->calculatePrices($blankoOjacanaIFalt, $vrednostBoda);
+
+        return view('pages.products.prices', [
+            'bananaBezOjacanja' => $bananaBezOjacanja,
+            'bananaOjacanaIliFleksibilna' => $bananaOjacanaIliFleksibilna,
+            'bananaOjacanaIliFleksibilnaIFalt' => $bananaOjacanaIliFleksibilnaIFalt,
+            'bananaBezOjacanjaIFalt' => $bananaBezOjacanjaIFalt,
+            'blankoBezOjacanja' =>  $blankoBezOjacanja,
+            'blankoOjacanaIFalt' => $blankoOjacanaIFalt
+        ]);
+    }
+
+    private function calculatePrices($nizKesa, $vrednostBoda)
+    {
+        foreach ($nizKesa as $kesa) {
+            $kesa->boja1 = $kesa->boja1 * $vrednostBoda;
+            if ($kesa->boja2) {
+                $kesa->boja2 = $kesa->boja2 * $vrednostBoda;
+                $kesa->boja3 = $kesa->boja3 * $vrednostBoda;
+                $kesa->boja4 = $kesa->boja4 * $vrednostBoda;
+                $kesa->boja5 = $kesa->boja5 * $vrednostBoda;
+            }
+        }
     }
 }
