31aee8dd459d3ede5980c78f73b0d1b0f00ee85f
Gringus0 <aleksandar.acko.jovanovic@gmail.com>
2025-05-13 15:58:28 +0200

    Sent Gallery image paths from DB to front

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,12 +9,67 @@ use App\Models\BananaOjacanaIliFleksibilnaIFalt;
 use App\Models\BlankoBezOjacanja;
 use App\Models\BlankoOjacanaIFalt;
 use App\Models\Bod;
+use App\Models\Gallery;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function listProductsWithGallery() {
-        return view('pages.products.products');
+        // reklamne kese -> 1
+        // trake za oznacavanje -> 2
+        // dzakovi -> 3
+        // strec folija -> 4
+        // zip kese -> 5
+        // tregerice -> 6
+        // OPS ambalaza -> 7
+        // air bubble folija -> 8
+        // PET ambalaza -> 9??? NEMA NISTA U BAZI NI NA SAJTU
+        // P.E. folije -> 10
+        // Plast. siroke potrosnje -> 11
+        // kese za zamrzivac -> 12
+        // slike iz proizvodnje -> 13
+        // sajmovi -> 14
+
+        try {
+            DB::beginTransaction();
+
+            $reklamneKese = Gallery::where('idgalerije', '1')->get();
+            $trakeZaOznacavanje = Gallery::where('idgalerije', '2')->get();
+            $dzakovi = Gallery::where('idgalerije', '3')->get();
+            $strecFolija = Gallery::where('idgalerije', '4')->get();
+            $zipKese = Gallery::where('idgalerije', '5')->get();
+            $tregerice = Gallery::where('idgalerije', '6')->get();
+            $opsAmbalaza = Gallery::where('idgalerije', '7')->get();
+            $airBubbleFolija = Gallery::where('idgalerije', '8')->get();
+            $petAmbalaza = Gallery::where('idgalerije', '9')->get();
+            $peFolija = Gallery::where('idgalerije', '10')->get();
+            $plastSirokePorosnje = Gallery::where('idgalerije', '11')->get();
+            $keseZaZamrzivac = Gallery::where('idgalerije', '12')->get();
+            $slikeIzProizvodnje = Gallery::where('idgalerije', '13')->get();
+            $sajmovi = Gallery::where('idgalerije', '14')->get();
+
+            DB::commit();
+        } catch(\Throwable $e) {
+            DB::rollBack();
+        }
+
+        return view('pages.products.products', [
+            'reklamneKese' => $reklamneKese,
+            'trakeZaOznacavanje' => $trakeZaOznacavanje,
+            'dzakovi' => $dzakovi,
+            'strecFolija' => $strecFolija,
+            'zipKese' => $zipKese,
+            'tregerice' => $tregerice,
+            'opsAmbalaza' => $opsAmbalaza,
+            'airBubbleFolija' => $airBubbleFolija,
+            'petAmbalaza' => $petAmbalaza,
+            'peFolija' => $peFolija,
+            'plastSirokePorosnje' => $plastSirokePorosnje,
+            'keseZaZamrzivac' => $keseZaZamrzivac,
+            'slikeIzProizvodnje' => $slikeIzProizvodnje,
+            'sajmovi' => $sajmovi
+        ]);
     }
 
     public function listProductPrices() {
--- resources/views/pages/products/products.blade.php
+++ resources/views/pages/products/products.blade.php
@@ -1,4 +1,23 @@
 @extends('layouts.layout')
 @section('content')
 Products & Gallery Page
+
+{{--'reklamneKese' => $reklamneKese,--}}
+{{--'trakeZaOznacavanje' => $trakeZaOznacavanje,--}}
+{{--'dzakovi' => $dzakovi,--}}
+{{--'strecFolija' => $strecFolija,--}}
+{{--'zipKese' => $zipKese,--}}
+{{--'tregerice' => $tregerice,--}}
+{{--'opsAmbalaza' => $opsAmbalaza,--}}
+{{--'airBubbleFolija' => $airBubbleFolija,--}}
+{{--'petAmbalaza' => $petAmbalaza,--}}
+{{--'peFolija' => $peFolija,--}}
+{{--'plastSirokePorosnje' => $plastSirokePorosnje,--}}
+{{--'keseZaZamrzivac' => $keseZaZamrzivac,--}}
+{{--'slikeIzProizvodnje' => $slikeIzProizvodnje,--}}
+{{--'sajmovi' => $sajmovi--}}
+
+{{--    @foreach($reklamneKese as $kesa)--}}
+{{--        {{$kesa->velikaslika}} <br>--}}
+{{--    @endforeach--}}
 @endsection
