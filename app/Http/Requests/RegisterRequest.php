acf0d5eaf68d8ef7bb77e4444a4844649ddb67a6
Gringus0 <aleksandar.acko.jovanovic@gmail.com>
2025-06-01 02:38:21 +0200

    Added a custom request for registration

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'phone' => ['required', 'string', 'regex:/^[0-9\s]+$/',
+                function ($attribute, $value, $fail) {
+                    $digitsOnly = preg_replace('/\D/', '', $value);
+                   $digitCount = strlen($digitsOnly);
+                   if ($digitCount < 9 || $digitCount > 11) {
+                       $fail('Telefon mora imati izmedju 9 i 11 cifara.');
+                   }
+            }],
+            'phone2' => ['nullable', 'string', 'regex:/^[0-9\s]+$/',
+                function ($attribute, $value, $fail) {
+                    $digitsOnly = preg_replace('/\D/', '', $value);
+                    $digitCount = strlen($digitsOnly);
+                    if ($digitCount < 9 || $digitCount > 11) {
+                        $fail('Telefon mora imati izmedju 9 i 11 cifara.');
+                    }
+                }],
+            'company' => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Ime je obavezno polje.',
+            'name.string' => 'Ime mora biti sačinjeno od slova.',
+            'name.max' => 'Ime ne sme biti duže od 255 slova.',
+            'email.required' => 'E-mail je obavezno polje.',
+            'email.string' => 'E-mail mora biti oblika primer@example.com',
+            'email.email' => 'E-mail mora biti oblika primer@example.com',
+            'email.max' => 'E-mail ne sme biti duži od 255 znakova.',
+            'phone.required' => 'Telefon je obavezno polje.',
+            'company.required' => 'Ime firme je obavezno polje.',
+            'company.string' => 'Firma mora biti sačinjeno od slova i znakova.',
+            'company.max' => 'Naziv firme ne sme biti duži od 255 slova, brojeva i/ili znakova.'
+        ];
+    }
+
+
+}
