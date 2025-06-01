17554f201fd0087d6324c38421917be916afb016
Gringus0 <aleksandar.acko.jovanovic@gmail.com>
2025-06-01 02:40:27 +0200

    Added functionality to the app for registering and login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
-    public function registerForm() {
-        return view('pages.auth.register');
+    public function registerAndLoginForm() {
+        return view('pages.auth.login-register');
     }
 
-    public function register(Request $request) {}
+    public function register(RegisterRequest $request) {
 
+        try {
+            DB::beginTransaction();
+            $user = new User;
+
+            $user->ime = $request->input('name');
+            $user->mail = $request->input('email');
+            $user->telefon = preg_replace('/\D/', '', $request->input('phone'));
+            $user->telefon2 = "";
+            if($request->input('phone2')) {
+                $user->telefon2 = preg_replace('/\D/', '', $request->input('phone2'));
+            }
+            $user->firma = $request->input('company');
+
+            $user->save();
+            DB::commit();
+            return back()->with('success', '<UNK> <UNK> <UNK> <UNK> <UNK> <UNK> <UNK>');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return  back()->with('error', $th->getMessage());
+        }
+
+
+
+    }
+
+    public function login(Request $request) {
+        $input = $request->input('login-email');
+
+        $user = User::where('mail', $input)->first();
+        If(!$user) {
+            return back()->with('error', 'Nema registracije sa datom email adresom.');
+        }
+        return back()->with('success', 'Login success!');
+    }
 
 }
