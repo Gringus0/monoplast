f4ffc5dcf72302d519a79e74851e6e3acbc394bd
Gringus0 <aleksandar.acko.jovanovic@gmail.com>
2025-06-03 14:18:04 +0200

    Adjusted validation rules and messages for login and registration

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class RegisterRequest extends FormRequest
 {
@@ -23,21 +24,21 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255'],
-            'phone' => ['required', 'string', 'regex:/^[0-9\s]+$/',
+            'email' => ['required', 'string', Rule::unique('clanovi', 'mail'), 'max:255'],
+            'phone' => ['required', 'string', 'regex:/^\+?[0-9\s]+$/',
                 function ($attribute, $value, $fail) {
                     $digitsOnly = preg_replace('/\D/', '', $value);
                    $digitCount = strlen($digitsOnly);
-                   if ($digitCount < 9 || $digitCount > 11) {
-                       $fail('Telefon mora imati izmedju 9 i 11 cifara.');
+                   if ($digitCount < 9 || $digitCount > 14) {
+                       $fail('Telefon mora imati izmedju 9 i 14 cifara.');
                    }
             }],
-            'phone2' => ['nullable', 'string', 'regex:/^[0-9\s]+$/',
+            'phone2' => ['nullable', 'string', 'regex:/^\+?[0-9\s]+$/',
                 function ($attribute, $value, $fail) {
                     $digitsOnly = preg_replace('/\D/', '', $value);
                     $digitCount = strlen($digitsOnly);
-                    if ($digitCount < 9 || $digitCount > 11) {
-                        $fail('Telefon mora imati izmedju 9 i 11 cifara.');
+                    if ($digitCount < 9 || $digitCount > 14) {
+                        $fail('Telefon mora imati izmedju 9 i 14 cifara.');
                     }
                 }],
             'company' => ['required', 'string', 'max:255'],
@@ -52,7 +53,7 @@ class RegisterRequest extends FormRequest
             'name.max' => 'Ime ne sme biti duže od 255 slova.',
             'email.required' => 'E-mail je obavezno polje.',
             'email.string' => 'E-mail mora biti oblika primer@example.com',
-            'email.email' => 'E-mail mora biti oblika primer@example.com',
+            'email.unique' => 'Već postoji nalog sa datom e-mail adresom.',
             'email.max' => 'E-mail ne sme biti duži od 255 znakova.',
             'phone.required' => 'Telefon je obavezno polje.',
             'phone.regex' => 'Telefon mora sadrzati samo cifre.',
