71a4129f98d7054f4ca65589d349d1877116dc42
Gringus0 <aleksandar.acko.jovanovic@gmail.com>
2025-06-05 21:24:17 +0200

    Added pagination to admin buyer-list.blade.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFive();
         if ($this->app->environment('local')) {
             Mail::alwaysTo('aleksandar.acko.jovanovic@gmail.com');
         }
--- resources/views/pages/admin/buyer-list.blade.php
+++ resources/views/pages/admin/buyer-list.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.admin')
 @section('content')
 Spisak Kupaca
+    @foreach($users as $user)
+        {{ $user->ime }} <br>
+    @endforeach
+    {{ $users->links() }}
 @endsection
